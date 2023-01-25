357adb65edb42e137218c11bbe5845cd425d23ff
jacker1342 <jackerchaiwan@gmail.com>
2023-01-25 23:35:31 +0700

    uploads main setup

---
+++ backend/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"github.com/jacker1342/se-65/controller"
+	"github.com/jacker1342/se-65/entity"
+	"github.com/jacker1342/se-65/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	entity.SetupDatabase()
+
+	r := gin.Default()
+	r.Use(CORSMiddleware())
+
+	api := r.Group("")
+	{
+		protected := api.Use(middlewares.Authorizes())
+		{
+			// User Routes
+			protected.GET("/users", controller.ListUsers)
+			protected.GET("/user/:id", controller.GetUser)
+			protected.PATCH("/users", controller.UpdateUser)
+			protected.DELETE("/users/:id", controller.DeleteUser)
+
+			// Manage Routes
+			protected.GET("/manages", controller.ListManages)
+			protected.GET("/manage/:id", controller.GetManage)
+			protected.POST("/manages", controller.CreateManage)
+			protected.PATCH("/manages", controller.UpdateManage)
+			protected.DELETE("/manages/:id", controller.DeleteManage)
+
+			// Category Routes
+			protected.GET("/categories", controller.ListCategories)
+			protected.GET("/category/:id", controller.GetCategory)
+			protected.POST("/categories", controller.CreateCategory)
+			protected.PATCH("/categories", controller.UpdateCategory)
+			protected.DELETE("/categories/:id", controller.DeleteCategory)
+
+			// Size Routes
+			protected.GET("/sizes", controller.ListSizes)
+			protected.GET("/size/:id", controller.GetSize)
+			protected.POST("/sizes", controller.CreateSize)
+			protected.PATCH("/sizes", controller.UpdateSize)
+			protected.DELETE("/sizes/:id", controller.DeleteSize)
+
+			// Lese Routes
+			protected.GET("/leases", controller.ListLeases)
+			protected.GET("/lease/:id", controller.GetLease)
+			protected.POST("/leases", controller.CreateLease)
+			protected.PATCH("/leases", controller.UpdateLease)
+			protected.DELETE("/leases/:id", controller.DeleteLease)
+
+			// Resident Routes
+			protected.GET("/residents", controller.ListResidents)
+			protected.GET("/resident/:id", controller.GetResident)
+			protected.POST("/residents", controller.CreateResident)
+			protected.PATCH("/residents", controller.UpdateResident)
+			protected.DELETE("/residents/:id", controller.DeleteResident)
+
+			// Room Routes
+			protected.GET("/rooms", controller.ListRooms)
+			protected.GET("/room/:id", controller.GetRoom)
+			protected.POST("/rooms", controller.CreateRoom)
+			protected.PATCH("/rooms", controller.UpdateRoom)
+			protected.DELETE("/rooms/:id", controller.DeleteRoom)
+
+			
+		}
+	}
+
+	// users Routes
+	r.POST("/users", controller.CreateUser)	
+
+	// Authentication Routes
+	r.POST("/login", controller.Login)
+
+	// Run the server
+	r.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS"{
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
